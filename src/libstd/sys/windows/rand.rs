4542a56614b9d5ab7a132f67d35a70b381d8318f
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-04-17 17:32:21 +0200

    libstd: windows: Use BCryptGenRandom on UWP
    Since RtlGenRandom isn't available

--- src/libstd/sys/windows/rand.rs
+++ src/libstd/sys/windows/rand.rs
@@ -1,7 +1,10 @@
 use crate::io;
 use crate::mem;
+#[cfg(target_os = "uwp")]
+use crate::ptr;
 use crate::sys::c;
 
+#[cfg(not(target_os = "uwp"))]
 pub fn hashmap_random_keys() -> (u64, u64) {
     let mut v = (0, 0);
     let ret = unsafe {
@@ -9,6 +12,21 @@ pub fn hashmap_random_keys() -> (u64, u64) {
                         mem::size_of_val(&v) as c::ULONG)
     };
     if ret == 0 {
+        panic!("couldn't generate random bytes: {}",
+               io::Error::last_os_error());
+    }
+    return v
+}
+
+#[cfg(target_os = "uwp")]
+pub fn hashmap_random_keys() -> (u64, u64) {
+    let mut v = (0, 0);
+    let ret = unsafe {
+        c::BCryptGenRandom(ptr::null_mut(), &mut v as *mut _ as *mut u8,
+                           mem::size_of_val(&v) as c::ULONG,
+                           c::BCRYPT_USE_SYSTEM_PREFERRED_RNG)
+    };
+    if ret != 0 {
         panic!("couldn't generate random bytes: {}",
                io::Error::last_os_error());
     }
